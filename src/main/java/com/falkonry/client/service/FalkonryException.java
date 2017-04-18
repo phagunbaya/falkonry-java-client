37b5b92e06bace8a1028683178004ed9004814f9
Chandresh Gurbani <chandresh-gurbani@falkonry.com>
2017-04-18 23:11:05 +0530

    refs #4863

---
+++ src/main/java/com/falkonry/client/service/FalkonryException.java
@@ -0,0 +1,13 @@
+package com.falkonry.client.service;
+
+import org.omg.CORBA.portable.ApplicationException;
+import org.omg.CORBA.portable.InputStream;
+
+public class FalkonryException extends Exception {
+	
+	public FalkonryException(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
